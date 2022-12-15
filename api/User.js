6cade8f28a872cd486a3f2360b85ce8689243af5
Elizabeth Ogunmola <98459984+Elizzy01@users.noreply.github.com>
2022-12-15 13:44:18 +0100

    added the signup function

--- api/User.js
+++ api/User.js
@@ -1,14 +1,103 @@
-const express = require ('express');
-const router = express.Router();
+// const express= require('express')
+// const router = express.Router()
 
-// Signup
-router.post('/signup', (req, res) => {
+// //mongodb user model
+// const User= require('./../models/User');
+// //password handler
+// const bcrypt =require('bcrypt')
 
-})
+// //signup
+// router.post('/signup', (req, res) => {
+//     let {name,email,password,dateOfBirth} = req.body
+//     name= name.trim()
+//     email=email.trim()
+//     password= password.trim()
+//     dateOfBirth= dateOfBirth.trim()
 
-// Signin
-router.post('/signin', (req, res) => {
+//     if (name == "" || email == "" || password == "" || dateOfBirth == "" ){
+//         res.json({
+//             status:"FAILED",
+//             message: "empty input field"
+//         });
+//     } else if (!/^[a-zA-Z]*$/.test(name)) {
+//         res.json({
+//             status: "FAILED",
+//             message: "Invalid name entered"
+//         })
+//     } else if ( /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+//         res.json({
+//             status: "FAILED",
+//             message: "Invalid input entered"
+//         })
+//     } else if (newDate(dateOfBirth).getTime()){
+//         res.json({
+//             status: "FAILED",
+//             message: "Invalid date of birth entered"
+//         })
+//     } else if (password.length < 8){
+//         res.json({
+//             status: "FAILED",
+//             message: "Password is too short"
+//         })
+//     } else {
+//         //checking if user already exists
+//     User.find({email}).then(result => {
+//         if(result.length){
+//             res.json({
+//                 status: "FAILED",
+//                 message: "User already exists"
+//             })
+//         } else {
 
-})
+//             //try to create new user
 
-module.exports = router;
+//             //password handling
+//             const saltRounds = 10;
+//             bcrypt.hash(password,saltRounds).then(hashedPassword => {
+//                 const newUser = new User({
+//                     name,
+//                     email,
+//                     password: hashedPassword,
+
+//                 });
+
+//                 newUser.save().then(result =>{
+//                     res.json({
+//                         status: "success",
+//                         message: "Signup sucessful",
+//                         data: result,
+//                     })
+//                 })
+//                 .catch(err => {
+//                     res.json({
+//                         status: "FAILED",       
+//                         message: "An error occurred while saving user account"
+//                     })
+//                 })
+
+//             })
+//             .catch(err=>{
+//                 res.json({
+//                     status:"FAILED",
+//                     message:"An error occured while hashing password"
+//                 }) 
+//             })
+//         }
+
+//         }).catch(err => {
+//             console.log(err)
+//             res.json({
+//                 status:"FAILED",
+//                 message:"An error occured while checking for existing user"
+//             })
+//         })
+//     }
+
+
+// })
+
+// //signin
+// router.post('/signin', (req,res)=> {
+
+// })
+// module.exports = router;
--- server.js
+++ server.js
@@ -1,60 +1,99 @@
 // mongodb
-require('./config/db');
+// require('./config/db');
 
 const express = require('express');
 const app = express();
 const PORT = 7000;
-
-const UserRouter = require('./api/User')
-
-// for accepting post from data
-const bodyParser = require('express').json;
-app.use(bodyParser());
-
-app.use('/user', UserRouter)
-
+var bodyParser=require("body-parser");
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+
+const uri = "mongodb+srv://UserDB:Nopoverty2022@no-poverty.phgzhfx.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.set('strictQuery', true);
+mongoose.connect(uri);
+var db=mongoose.connection;
+db.on('error', console.log.bind(console, "connection error"));
+db.once('open', function(callback){
+    console.log("connection succeeded");
+})
+  
+  
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+  
+
+// const UserRouter = require('./api/User')
+
+// // for accepting post from data
+// const bodyParser = require('express').json;
+// app.use(bodyParser());
+
+// app.use('/user', UserRouter)
+
 app.use(express.static('public'));
 
-// passport -Google authentication
-const passport = require('passport');
-const cookieSession = require('cookie-session');
-require('./passport');
-  
-app.use(cookieSession({
-    name: 'google-auth-session',
-    keys: ['key1', 'key2']
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+// signup Form
+app.post('/form_data', function(req, res){
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
 
-app.get('/', (req, res) => {
-    res.send("<button><a href='/auth'>Login With Google</a></button>")
-});
-  
-// Auth 
-app.get('/auth' , passport.authenticate('google', { scope:
-    [ 'email', 'profile' ]
-}));
-  
-// Auth Callback
-app.get( '/auth/callback',
-    passport.authenticate( 'google', {
-        successRedirect: '/auth/callback/success',
-        failureRedirect: '/auth/callback/failure'
-}));
-  
-// Success 
-app.get('/auth/callback/success' , (req , res) => {
-    if(!req.user)
-        res.redirect('/auth/callback/failure');
-    res.send("Welcome " + req.user.email);
-});
-  
-// failure
-app.get('/auth/callback/failure' , (req , res) => {
-    res.send("Error");
+    var data ={
+        "name": name,
+        "email": email,
+        "password": password,
+    }
+
+    db.collection('details').insertOne(data,function(err, collection){
+        if (err) throw err;
+        console.log("Record inserted Successfully");
+              
+    });
 })
+
+// // passport -Google authentication
+// const passport = require('passport');
+// const cookieSession = require('cookie-session');
+// require('./passport');
+  
+// app.use(cookieSession({
+//     name: 'google-auth-session',
+//     keys: ['key1', 'key2']
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// app.get('/', (req, res) => {
+//     res.send("<button><a href='/auth'>Login With Google</a></button>")
+// });
+  
+// // Auth 
+// app.get('/auth' , passport.authenticate('google', { scope:
+//     [ 'email', 'profile' ]
+// }));
+  
+// // Auth Callback
+// app.get( '/auth/callback',
+//     passport.authenticate( 'google', {
+//         successRedirect: '/auth/callback/success',
+//         failureRedirect: '/auth/callback/failure'
+// }));
+  
+// // Success 
+// app.get('/auth/callback/success' , (req , res) => {
+//     if(!req.user)
+//         res.redirect('/auth/callback/failure');
+//     res.send("Welcome " + req.user.email);
+// });
+  
+// // failure
+// app.get('/auth/callback/failure' , (req , res) => {
+//     res.send("Error");
+// })
 
 app.use('/css', express.static(__dirname +'public/css'))
 app.use('/js', express.static(__dirname +'public/js'))
